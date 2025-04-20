6c033543103f0aaa0868951f824bca62813cf81a
Sreya Saju <68282466+sreyasaju@users.noreply.github.com>
2025-04-20 10:11:06 +0300

    fix: trailing accent colors

--- main.js
+++ main.js
@@ -1,34 +1,36 @@
 document.addEventListener('DOMContentLoaded', async () => {
     const tableBody = document.getElementById('topic-table-body');
-  
+    
     const topics = await fetch('/topics.json').then(res => res.json());
-  
+    
     // dummy selected data (replace with Firebase later)
     const selected = {
       "1": { name: "Sreya Saju", class: "12B", adminNo: "12343" },
       "5": { name: "John Doe", class: "11B", adminNo: "21147" }
     };
-
+  
     const colorMap = {
-        Environment: "is-success",
-        Technology: "is-info",
-        Health: "is-danger",
-        Physics: "is-primary",
-        Chemistry: "is-primary",
-        Sustainability: "is-warning"
-      };
-      
-    const subject = topic.subject;
-    const subjectColor = colorMap[subject] || "is-light";
-
+        environment: "is-success",
+        technology: "is-info",
+        health: "is-danger",
+        physics: "is-primary",
+        chemistry: "is-primary",
+        sustainability: "is-warning",
+        biology: "is-link"
+    };
+    
     topics.forEach((topic, index) => {
       const sel = selected[topic.id];
       const tr = document.createElement('tr');
   
+      const normalizedSubject = topic.subject.trim().toLowerCase();
+      console.log("Normalized subject:", normalizedSubject);
+      const subjectColor = colorMap[normalizedSubject] || "is-light";
+  
       tr.innerHTML = `
         <td>${index + 1}</td>
         <td>${topic.name}</td>
-        <td><span class="tag ${subjectColor}">${subject}</span></td>
+        <td><span class="tag ${subjectColor}">${topic.subject}</span></td>
         <td>
           ${
             sel
@@ -43,7 +45,7 @@ document.addEventListener('DOMContentLoaded', async () => {
           }
         </td>
       `;
-  
+    
       tableBody.appendChild(tr);
     });
   });
